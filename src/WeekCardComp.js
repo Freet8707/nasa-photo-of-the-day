5a414d2fed061a4f7243391b9ed1d12a177ad6fd
Jeremy Freetly <freet8707@gmail.com>
2020-05-26 22:33:49 -0500

    restoring styles

---
+++ src/WeekCardComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card(props){
+    return(
+        <div>
+            <h3>Date Here</h3>
+            <img></img>
+        </div>
+    )
+}
+
+export default Card
