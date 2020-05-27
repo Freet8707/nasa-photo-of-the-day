074e5676a2dbdd22947026e206f44d65eabefcb4
Jeremy Freetly <freet8707@gmail.com>
2020-05-27 01:41:29 -0500

    five images pulled from last weeks apod api and passed into card creator components

--- src/WeekCardComp.js
+++ src/WeekCardComp.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function WeekCardComp(props){
-    console.log('inside card component')
+    const photo = props.photo
+    const date = props.date
+    console.log(photo)
     return(
         
         <div className='picture-card'>
+            <h3>{date}</h3>
+            <img className='photo-fit' src={photo} alt='nasa photo' />
         </div>
     )
 }
--- src/WeekCards.js
+++ src/WeekCards.js
@@ -3,46 +3,51 @@ import WeekCardComp from './WeekCardComp'
 import axios from 'axios'
 
 function WeekCards(){
-    const dateRange = ['2020-05-18', '2020-05-19', '2020-05-20', '2020-05-21', '2020-05-22']
-    const [photo1, setPhoto1] = useState([])
-    const [photo2, setPhoto2] = useState([])
-    const [photo3, setPhoto3] = useState([])
-    const [photo4, setPhoto4] = useState([])
-    const [photo5, setPhoto5] = useState([])
+    // const dateRange = ['2020-05-18', '2020-05-19', '2020-05-20', '2020-05-21', '2020-05-22']
+    const [photo1, setPhoto1] = useState('')
+    const [photo2, setPhoto2] = useState('')
+    const [photo3, setPhoto3] = useState('')
+    const [photo4, setPhoto4] = useState('')
+    const [photo5, setPhoto5] = useState('')
 
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?date=${dateRange[0]}&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
-        .then(response => setPhoto1(response))
+        axios.get(`https://api.nasa.gov/planetary/apod?date=2020-05-18&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
+        .then(response => setPhoto1(response.data.url))
         .catch(error => console.log('error', error))
     }, [])
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?date=${dateRange[1]}&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
-        .then(response => setPhoto2(response))
+        axios.get(`https://api.nasa.gov/planetary/apod?date=2020-05-19&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
+        .then(response => setPhoto2(response.data.url))
         .catch(error => console.log('error', error))
     }, [])
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?date=${dateRange[2]}&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
-        .then(response => setPhoto3(response))
+        axios.get(`https://api.nasa.gov/planetary/apod?date=2020-05-20&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
+        .then(response => setPhoto3(response.data.url))
         .catch(error => console.log('error', error))
     }, [])
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?date=${dateRange[3]}&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
-        .then(response => setPhoto4(response))
+        axios.get(`https://api.nasa.gov/planetary/apod?date=2020-05-21&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
+        .then(response => setPhoto4(response.data.url))
         .catch(error => console.log('error', error))
     }, [])
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?date=${dateRange[4]}&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
-        .then(response => setPhoto5(response))
+        axios.get(`https://api.nasa.gov/planetary/apod?date=2020-05-22&api_key=xpfLwc9FBMcUvKuj84a1BlSh89ALxN4mCCdfMCyY`)
+        .then(response => setPhoto5(response.data.url))
         .catch(error => console.log('error', error))
     }, [])
 
     return(
+        <div>
+        <div>
+            <h1>Last Week's Photos!</h1>
+        </div>
         <div className='cards-container'>
-            <WeekCardComp photo={photo1} />
-            <WeekCardComp photo={photo2} />
-            <WeekCardComp photo={photo3} />
-            <WeekCardComp photo={photo4} />
-            <WeekCardComp photo={photo5} />
+            <WeekCardComp photo={photo1} date='May 18, 2020' />
+            <WeekCardComp photo={photo2} date='May 19, 2020' />
+            <WeekCardComp photo={photo3} date='May 20, 2020' />
+            <WeekCardComp photo={photo4} date='May 21, 2020' />
+            <WeekCardComp photo={photo5} date='May 22, 2020' />
+        </div>
         </div>
     )
 }
